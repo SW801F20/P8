aec5e2616b75ecd121a4c52ab5e57f1a71672d52
KVISDAOWNER <krijen16@student.aau.dk>
2020-03-18 10:45:36 +0100

    Enable discoverability added

--- DeadCrumbs/app/src/main/java/dead/crumbs/ui/MainActivity.kt
+++ DeadCrumbs/app/src/main/java/dead/crumbs/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package dead.crumbs.ui
 
 import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,22 +18,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_ENABLE_BT = 1
-
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializeUi()
 
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-        if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-            throw Exception("Device doesn't support Bluetooth")
-        }
-
-        if (bluetoothAdapter?.isEnabled == false) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-        }
+        enableBluetooth()
+        enableBluetoothDiscoverable()
     }
 
     private fun initializeUi() {
@@ -66,7 +61,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
@@ -76,6 +70,39 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
             var t = Toast.makeText(this@MainActivity,  "Bluetooth Enabled!", Toast.LENGTH_LONG)
             t. show()
+        }
+    }
+
+    private fun enableBluetooth() {
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            throw Exception("Device doesn't support Bluetooth")
+        }
+
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
+    private fun enableBluetoothDiscoverable() {
+        val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+        startActivity(discoverableIntent)
+        val intentFilter = IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    private val broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(
+            context: Context,
+            intent: Intent
+        ) {
+            val action = intent.action
+            if (action == BluetoothAdapter.ACTION_SCAN_MODE_CHANGED) {
+                val mode =
+                    intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR)
+            }
         }
     }
 
