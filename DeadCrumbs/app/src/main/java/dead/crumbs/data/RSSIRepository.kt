3daadec701eadfb53fb0b05542cc29ea902c0f1a
Kristoffer Skagbæk Jensen <krijen16@student.aau.dk>
2020-03-19 12:39:48 +0100

    Feature: MVVM (#8)
    MVVM feature

---
+++ DeadCrumbs/app/src/main/java/dead/crumbs/data/RSSIRepository.kt
@@ -0,0 +1,24 @@
+package dead.crumbs.data
+
+import dead.crumbs.data.DAO.RSSI_DAO
+
+class RSSIRepository private constructor(private val rssiDao: RSSI_DAO) {
+
+    // This may seem redundant.
+    // Imagine a code which also updates and checks the backend.
+    fun addRSSI(rssi: RSSI) {
+        rssiDao.add_rssi(rssi)
+    }
+
+    fun getRSSIs() = rssiDao.getRSSIs()
+
+    companion object {
+        // Singleton instantiation you already know and love
+        @Volatile private var instance: RSSIRepository? = null
+
+        fun getInstance(rssiDao: RSSI_DAO) =
+            instance ?: synchronized(this) {
+                instance ?: RSSIRepository(rssiDao).also { instance = it }
+            }
+    }
+}
---
+++ DeadCrumbs/app/src/main/java/dead/crumbs/ui/rssiViewModelFactory.kt
@@ -0,0 +1,14 @@
+package dead.crumbs.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dead.crumbs.data.RSSIRepository
+
+class RSSIViewModelFactory(private val rssiRepository: RSSIRepository)
+    : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return RSSIViewModel(rssiRepository) as T
+    }
+}
