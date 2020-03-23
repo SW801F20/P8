2326824414737db203297b64a36951b02846c139
Kristoffer Skagbæk Jensen <krijen16@student.aau.dk>
2020-03-23 08:36:07 +0100

    Feature Bluetooth RSSI (#14)
    Bluetooth RSSI

---
+++ DeadCrumbs/app/src/main/java/dead/crumbs/ui/BluetoothActivity.kt
@@ -0,0 +1,122 @@
+package dead.crumbs.ui
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dead.crumbs.data.BluetoothRSSI
+import java.util.*
+import kotlin.concurrent.timer
+
+
+//factory: RSSIViewModelFactory, viewModel: RSSIViewModel
+class BluetoothActivity() : Activity(){
+    private var factory: RSSIViewModelFactory? = null
+    private var viewModel: RSSIViewModel? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupBluetooth()
+    }
+
+    private val bluetoothAdapter : BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String = intent.action!!
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    var bluetooth_rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE) // rssi
+                    MainActivity.receiveRSSI( BluetoothRSSI(bluetooth_rssi, device!!.address))
+                }
+            }
+        }
+    }
+    private val receiver2 = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String = intent.action!!
+            when(action) {
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED->{
+                    discover()
+                }
+            }
+        }
+    }
+
+    fun setupBluetooth(){
+        //Get bluetooth adapter
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            throw Exception("Device doesn't support Bluetooth")
+        }
+
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        //val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        registerReceiver(receiver, filter)
+
+        val filter2 = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        //val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        registerReceiver(receiver2, filter2)
+
+        discover()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver)
+        unregisterReceiver(receiver2)
+    }
+
+    private fun discover(){
+        //Check if App has been granted necessary permission, if not request them
+        checkBTPermissions()
+
+        if (bluetoothAdapter!!.isDiscovering) {
+            //Restart discovery
+            bluetoothAdapter.cancelDiscovery()
+            bluetoothAdapter.startDiscovery()
+        }
+        else{
+            //Start discovery
+            if(!bluetoothAdapter.startDiscovery())
+                throw java.lang.Exception("Bluetooth StartDiscovery Failed")
+        }
+    }
+
+    private fun checkBTPermissions() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            var permissionCheck =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION")
+                } else {
+                    TODO("VERSION.SDK_INT < M")
+                }
+            permissionCheck += checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION")
+            if (permissionCheck != 0) {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 1001
+                ) //Any number
+            }
+        }
+    }
+
+}
--- DeadCrumbs/app/src/main/java/dead/crumbs/ui/MainActivity.kt
+++ DeadCrumbs/app/src/main/java/dead/crumbs/ui/MainActivity.kt
@@ -1,51 +1,95 @@
 package dead.crumbs.ui
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import dead.crumbs.R
-import dead.crumbs.data.RSSI
+import dead.crumbs.data.BluetoothRSSI
 import dead.crumbs.utilities.InjectorUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val REQUEST_ENABLE_BT = 1
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializeUi()
+
+        enableBluetooth()
+
+        //----This enables Bluetooth Discoverability for 1 hour
+        enableBluetoothDiscoverability()
+        startBluetoothScan()
     }
 
+    //------------Bluetooth Part-------------//
+    private fun enableBluetoothDiscoverability() {
+        val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3600) // 3600 = 1 hour should make it not timeout
+        }
+        startActivity(discoverableIntent)
+    }
+
+    private fun enableBluetooth(){
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            throw Exception("Device doesn't support Bluetooth")
+        }
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
+    private fun startBluetoothScan(){
+        val intent = Intent(this, BluetoothActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object{
+        var viewModel : RSSIViewModel? = null
+        fun receiveRSSI(rssi : BluetoothRSSI){
+            if (rssi != null){
+                viewModel!!.addRSSI(rssi)
+            }
+        }
+    }
+
+
+    //----------Initialization of MainActivity UI----------//
     private fun initializeUi() {
         // Get the rssisViewModelFactory with all of it's dependencies constructed
         val factory = InjectorUtils.provideRSSIViewModelFactory()
         // Use ViewModelProviders class to create / get already created rssisViewModel
         // for this view (activity)
-        val viewModel = ViewModelProviders.of(this, factory)
+        viewModel = ViewModelProviders.of(this, factory)
             .get(RSSIViewModel::class.java)
 
         // Observing LiveData from the RSSIViewModel which in turn observes
         // LiveData from the repository, which observes LiveData from the DAO ☺
-        viewModel.getRSSIs().observe(this, Observer { RSSIs ->
+        viewModel!!.getRSSIs().observe(this, Observer { RSSIs ->
             val stringBuilder = StringBuilder()
             RSSIs.forEach { rssi ->
                 stringBuilder.append("$rssi\n\n")
             }
             textView.text = stringBuilder.toString()
         })
+    }
 
-        // When button is clicked, instantiate a rssi and add it to DB through the ViewModel
-        button.setOnClickListener {
-            val rssi_temp : RSSI = RSSI(1.0, 1.0, "hello")
-            viewModel.addRSSI(rssi_temp)
-            textView.text = rssi_temp.toString()
-            /*
-            val rssi = RSSI(editText_rssi.text.toString(), editText_author.text.toString())
-            viewModel.addrssi(rssi)
-            editText_rssi.setText("")
-            editText_author.setText("")
-            */
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
+            var t = Toast.makeText(this@MainActivity,  "Bluetooth Enabled!", Toast.LENGTH_LONG)
+            t. show()
         }
     }
 
