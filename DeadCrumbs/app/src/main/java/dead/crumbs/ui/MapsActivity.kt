9931d4122f9651343f09d084d4dd2225423a5050
David Olesen Braae <davidbraae@hotmail.com>
2020-04-05 14:57:48 +0200

    Added button for opening map

--- DeadCrumbs/app/src/main/java/dead/crumbs/ui/MapsActivity.kt
+++ DeadCrumbs/app/src/main/java/dead/crumbs/ui/MapsActivity.kt
@@ -26,7 +26,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.button_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
