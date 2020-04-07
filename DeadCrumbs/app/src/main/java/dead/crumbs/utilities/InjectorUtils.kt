7841a34ff93000e8c3d1041f4a093d4b12608361
KVISDAOWNER <krijen16@student.aau.dk>
2020-04-07 11:48:41 +0200

    InjectorUtils update

--- DeadCrumbs/app/src/main/java/dead/crumbs/utilities/InjectorUtils.kt
+++ DeadCrumbs/app/src/main/java/dead/crumbs/utilities/InjectorUtils.kt
@@ -3,7 +3,7 @@ package dead.crumbs.utilities
 import dead.crumbs.data.FakeDatabase
 import dead.crumbs.data.MapsRepository
 import dead.crumbs.data.RSSIRepository
-import dead.crumbs.ui.MapsViewModelFactory
+import dead.crumbs.ui.SingletonMapsViewModelFactory
 import dead.crumbs.ui.RSSIViewModelFactory
 
 object InjectorUtils {
@@ -16,11 +16,17 @@ object InjectorUtils {
         return RSSIViewModelFactory(rssiRepository)
     }
 
+    var singletonMapsViewModelFactory: SingletonMapsViewModelFactory? = null
+
     // This will be called from ?
-    fun provideMapsViewModelFactory(): MapsViewModelFactory {
-        // ViewModelFactory needs a repository, which in turn needs a DAO from a database
-        // The whole dependency tree is constructed right here, in one place
-        val mapsRepository = MapsRepository.getInstance(FakeDatabase.getInstance().mapsDao)
-        return MapsViewModelFactory(mapsRepository)
+    fun singletonProvideMapsViewModelFactory(): SingletonMapsViewModelFactory {
+        if (singletonMapsViewModelFactory == null)
+        {
+            // ViewModelFactory needs a repository, which in turn needs a DAO from a database
+            // The whole dependency tree is constructed right here, in one place
+            val mapsRepository = MapsRepository.getInstance(FakeDatabase.getInstance().mapsDao)
+            singletonMapsViewModelFactory = SingletonMapsViewModelFactory(mapsRepository)
+        }
+        return singletonMapsViewModelFactory as SingletonMapsViewModelFactory
     }
 }
