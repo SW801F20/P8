652b856b79cef3255eadf9d2f589c3e4525f132a
KVISDAOWNER <krijen16@student.aau.dk>
2020-05-08 16:41:27 +0200

    Tobias' and Bjarke's getDateTime() AM PM fix

--- DeadCrumbs/app/src/main/java/dead/crumbs/utilities/UtilFunctions.kt
+++ DeadCrumbs/app/src/main/java/dead/crumbs/utilities/UtilFunctions.kt
@@ -8,7 +8,7 @@ class UtilFunctions() {
             val currYear = Calendar.getInstance().get(Calendar.YEAR).toString().padStart(4,'0')
             val currMonth = (Calendar.getInstance().get(Calendar.MONTH) + 1).toString().padStart(2,'0')
             val currDate = Calendar.getInstance().get(Calendar.DATE).toString().padStart(2,'0')
-            val currHour = Calendar.getInstance().get(Calendar.HOUR).toString().padStart(2,'0')
+            val currHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY).toString().padStart(2,'0')
             val currMinute = Calendar.getInstance().get(Calendar.MINUTE).toString().padStart(2,'0')
             val currSecond = Calendar.getInstance().get(Calendar.SECOND).toString().padStart(2,'0')
             val dateTimeString = currYear + "-" + currMonth + "-" + currDate+ "T" + currHour + ":" + currMinute + ":" + currSecond
